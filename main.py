74fc4d4964b7aeeb1a8754cc648c0a3bf8f8c350
mdkoe <pham_hong_thanh@mediado.jp>
2018-08-30 14:45:52 +0900

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import getopt
+import zipfile
+from pathlib import Path
+import os
+import image_slicer
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    print(filename)
+
+    zipFile = zipfile.ZipFile(filename,'r')
+    for imageName in zipFile.namelist():
+        path = Path(imageName.encode('cp437').decode('cp932'))
+        if imageName[-1] == '/': #folder
+            if not path.exists():
+                path.mkdir()
+        else:
+            with path.open('wb') as folder:
+                folder.write(zipFile.read(imageName))
+            image_slicer.slice(path,2)
+            os.remove(path)
+    zipFile.close()
