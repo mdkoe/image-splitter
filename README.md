2829d8d418d74d568bbd60afd0f814cb87e6494a
mdkoe <pham_hong_thanh@mediado.jp>
2018-08-31 18:58:41 +0900

    add feature add option in commad

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Image-splitter
 
 ## インストール方法
@@ -16,6 +17,15 @@ brew install imagemagick
 ## Usage
 コンマンドに以下の通りに実行して下さい
 ```
-python3 main.py filename.zip
+python3 main.py filename.zip [output_path]
 ```
 ![alt text](https://sv1.uphinhnhanh.com/images/2018/08/30/Aug-30-201816-52-43.gif)
+# Option
+You can custom splice image with after properties
+| Property | Values | Default | Preview
+| ------- | ------- |------- | -------
+| --help |  |  | |
+| --log   | True/False | False |
+| --png   | True/False | False|
+| --jpg   | True/False | True |
+| --quality  | 1...100 | 85 |
--- main.py
+++ main.py
@@ -1,47 +1,47 @@
 import sys
 import getopt
-import zipfile
-import os
-import subprocess
-from pathlib import Path
-from helpers import convert_path
+from helpers import *
+from splicer import process
 
-def slice(path):
-    imageName = os.path.splitext(path)[0]
+def main(argv):
+    # default value
+    ext = '.jpg'
+    quality = 85
+    log = False
+    out_path = ''
 
-    #get size
-    size = subprocess.check_output(["identify", "-format", "%w %h", path])
-    size = list(map(int, size.decode('utf-8').split(" ")))
+    # commad split
+    try:
+        opts, args = getopt.getopt(argv,'',['help','jpg','png','log','quality='])
+    except getopt.GetoptError:
+        show_error(argv)
+        sys.exit()
 
-    # calculator size
-    image_w, image_h = size
-    splice_w = int(image_w/2)
-    splice_h = int(image_h)
-
-    #slice-left
-    leftArea = (convert_path(str(path)), splice_w, splice_h, 0, 0, convert_path(imageName)+"b.jpg")
-    leftCmd = "convert %s -crop '%dx%d+%d+%d' %s"%leftArea
-    os.system(leftCmd)
-
-    #slice-right
-    rightArea = (convert_path(str(path)), image_w - splice_w, image_h, splice_w, 0, convert_path(imageName)+"a.jpg")
-    rightCmd = "convert %s -crop '%dx%d+%d+%d' %s"%rightArea
-    os.system(rightCmd)
+    # commad process
+    for opt,value in opts:
+        if opt == '--help':
+            show_help()
+            sys.exit(1)
+        if opt == '--log':
+            log = True
+        if opt == '--jpg':
+            ext = '.jpg'
+        if opt == '--png':
+            ext = '.png'
+        if opt == '--quality':
+            quality = int(value)
+            if quality<0 or quality>100:
+                show_error_quality()
+                sys.exit(2)
+    if len(args) == 0 or len(args) > 2:
+        show_error(argv)
+        sys.exit()
+    if len(args) == 2:
+        out_path = args[1]
+    input_path= args[0]
+    
+    process(input_path,out_path,ext,quality,log)
+    show_help()
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-
-    zipFile = zipfile.ZipFile(filename,'r')
-    for imageName in zipFile.namelist():
-        path = Path(imageName.encode('cp437').decode('cp932'))
-        if imageName[-1] == '/': #folder
-            if not path.exists():
-                path.mkdir()
-        else:
-            with path.open('wb') as folder:
-                folder.write(zipFile.read(imageName))
-            print(path)
-            slice(path)
-            os.remove(path)
-    zipFile.close()
-    print("Complete")
+    main(sys.argv[1:])
